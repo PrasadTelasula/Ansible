ac9cc44b1857362cb62e135e88dc2ba7eb322cf7
Prasad Telasula <Prasad.Telasula@yahoo.com>
2020-07-01 16:10:06 +0530

    Added code

--- provider.tf
+++ provider.tf
@@ -2,6 +2,16 @@ provider "aws" {
    region = "ap-south-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform-cicd-stat-file"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "terraform-cicd-state"
+ }
+}
+
+
 provider "template" {
   #version = "~> 0.1"
 }
